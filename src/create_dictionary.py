c542f1e82ddcb7132d85118a8bc5e2b45ba75e09
Moritz <60201269+clrfl@users.noreply.github.com>
2023-08-04 11:23:32 +0200

    Update create_dictionary.py
    typo

--- src/create_dictionary.py
+++ src/create_dictionary.py
@@ -2,7 +2,7 @@
 # This code can be used to create a dictionary of { from-id : username }, when compensating for deleted accounts.
 # However, in most cases this won't be necessary.
 #
-# When creating this dictionary files, all known names are filled in automatically from your database file.
+# When creating this dictionary file, all known names are filled in automatically from your database file.
 # You can fill in additional usernames manually (if you can restore them from context) by
 # replacing the "none"s in the dictionary.json file.
 # You then need to set the parameter read_file( ..., use_dictionary=True)
